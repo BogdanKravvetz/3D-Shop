999f5858c9f8133b2503744d9e95d2f66352dfef
BogdanKravvetz <44473395+BogdanKravvetz@users.noreply.github.com>
2020-12-08 00:41:42 +0200

    server setup

---
+++ server/src/app.js
@@ -0,0 +1,15 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+app.listen(8081);
+
+app.get('/status', (req,res) =>{
+    res.send({message: "Yah, it's working!"});
+});
